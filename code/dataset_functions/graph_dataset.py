8564558bb9eef6c1ec145487dd2d86c3e3f09bcf
Ron Kantorovich <ron.ka@nlp-2080-1.csf.technion.ac.il>
2021-07-27 18:16:04 +0300

    bens code replaced our code

--- code/dataset_functions/graph_dataset.py
+++ code/dataset_functions/graph_dataset.py
@@ -1,57 +1,73 @@
 from dataset_functions.twitter_dataset import TwitterDataset
-from classes.basic_classes import DatasetType, DataSet
+from classes.basic_classes import DataSet
 from helpers.getGitPath import getGitPath
 
 from typing import NamedTuple
 import os.path as osp
 import pickle
 import torch
+import torch_geometric
 from torch_geometric.datasets import Planetoid
 
-import numpy as np
 
 class Masks(NamedTuple):
+    """
+        a Mask object with the following fields:
+    """
     train: torch.tensor
     val: torch.tensor
     test: torch.tensor
 
 
 class GraphDataset(object):
-    def __init__(self, dataset, device):
+    """
+        a base class for datasets
+
+        Parameters
+        ----------
+        dataset: DataSet
+        device: torch.device
+    """
+    def __init__(self, dataset: DataSet, device: torch.device):
         super(GraphDataset, self).__init__()
         name = dataset.string()
         self.name = name
         self.device = device
 
         data = self._loadDataset(dataset, device)
-        self._setMasks(data, name)
+
+        if dataset is DataSet.TWITTER:
+            train_mask, val_mask, test_mask = torch.load('../masks/twitter.dat')
+            setattr(data, 'train_mask', train_mask)
+            setattr(data, 'val_mask', val_mask)
+            setattr(data, 'test_mask', test_mask)
+            setattr(data, 'test_mask', test_mask)
+            self.num_features = 200
+            self.num_classes = 2
+        else:
+            self._setMasks(data, name)
+
         self._setReversedArrayList(data)
 
         self.data = data
         self.type = dataset.get_type()
-        self.skip_attributes = True if dataset.get_type() is DatasetType.CONTINUOUS else False
 
-    def _loadDataset(self, dataset, device):
+    def _loadDataset(self, dataset: DataSet, device: torch.device) -> torch_geometric.data.Data:
+        """
+            a loader function for the requested dataset
+        """
         dataset_path = osp.join(getGitPath(), 'datasets')
         if dataset is DataSet.PUBMED or dataset is DataSet.CORA or dataset is DataSet.CITESEER:
             dataset = Planetoid(dataset_path, dataset.string())
         elif dataset is DataSet.TWITTER:
             twitter_glove_path = osp.join(dataset_path, 'twitter', 'glove.pkl')
             if not osp.exists(twitter_glove_path):
-                quit("Go to README and follow the download instructions to the TWITTER dataset")
+                exit("Go to README and follow the download instructions to the TWITTER dataset")
             else:
                 dataset = TwitterDataset(osp.dirname(twitter_glove_path))
                 with open(twitter_glove_path, 'rb') as file:
                     glove_matrix = pickle.load(file)
                 self.glove_matrix = torch.tensor(glove_matrix, dtype=torch.float32).to(device)
-        # elif dataset is DataSet.PPI:
-        #     PPI_path = osp.join(datasets_path, 'PPI')
-        #     data_split = []
-        #     for split in ('train', 'val', 'test'):
-        #         PPI.url = 'https://data.dgl.ai/dataset/ppi.zip'
-        #         data_split.append(PPI(PPI_path, split=split))
-        # elif dataset is DataSet.FLICKR:
-        #     dataset = Flickr(osp.join(datasets_path, 'Flickr'))
 
         data = dataset[0].to(self.device)
         setattr(data, 'num_classes', dataset.num_classes)
@@ -60,13 +76,19 @@ class GraphDataset(object):
         self.num_classes = dataset.num_classes
         return data
 
-    def _setMasks(self, data, name):
-        # if True:
+    def _setMasks(self, data: torch_geometric.data.Data, name: str):
+        """
+            sets train,val and test mask for the data
+
+            Parameters
+            ----------
+            data: torch_geometric.data.Data
+            name: str
+        """
         if not hasattr(data, 'train_mask') or not hasattr(data, 'val_mask') or not hasattr(data, 'test_mask'):
             self.train_percent = train_percent = 0.1
             self.val_percent = val_percent = 0.3
-            self.seed = seed = 5 # RGG
-            masks = GraphDataset._generateMasks(data, name, train_percent, val_percent, seed=seed)
+            masks = self._generateMasks(data, name, train_percent, val_percent)
         else:
             masks = Masks(data.train_mask, data.val_mask, data.test_mask)
 
@@ -74,43 +96,33 @@ class GraphDataset(object):
         setattr(data, 'val_mask', masks.val)
         setattr(data, 'test_mask', masks.test)
 
-    @staticmethod
-    def _generateMasks(data, name, train_percent, val_percent, num_nodes=None, num_classes=None, labels=None, seed = None):
-        if num_nodes is None:
-            eff_num_nodes = data.num_nodes
-        else:
-            eff_num_nodes = num_nodes
+    def _generateMasks(self, data: torch_geometric.data.Data, name: str, train_percent: float, val_percent: float)\
+            -> Masks:
+        """
+            generates train,val and test mask for the data
 
-        train_mask = torch.zeros(eff_num_nodes).type(torch.bool)
-        val_mask = torch.zeros(eff_num_nodes).type(torch.bool)
-        test_mask = torch.zeros(eff_num_nodes).type(torch.bool)
-        
-        if num_classes is None:
-            eff_num_classes = data.num_classes
-        else:
-            eff_num_classes = num_classes
-        
-        if labels is None:
-            eff_labels = data.y
-        else:
-            eff_labels = labels
+            Parameters
+            ----------
+            data: torch_geometric.data.Data
+            name: str
+            train_percent: float
+            val_percent: float
+
+            Returns
+            -------
+            masks: Masks
+        """
+        train_mask = torch.zeros(data.num_nodes).type(torch.bool)
+        val_mask = torch.zeros(data.num_nodes).type(torch.bool)
+        test_mask = torch.zeros(data.num_nodes).type(torch.bool)
+
         # taken from Planetoid
-        for c in range(eff_num_classes):
-            idx = (eff_labels == c).nonzero(as_tuple=False).view(-1)
+        for c in range(data.num_classes):
+            idx = (data.y == c).nonzero(as_tuple=False).view(-1)
             num_train_per_class = round(idx.size(0) * train_percent)
             num_val_per_class = round(idx.size(0) * val_percent)
 
-            ############## BEN ###############
             idx_permuted = idx[torch.randperm(idx.size(0))]
-            ##################################
-
-            ############## RGG ###############
-            # if seed is not None:
-            #     np.random.seed(seed)
-                
-            # idx_permuted = torch.Tensor(np.random.permutation(idx.cpu())).long()
-            ##################################
-
             train_idx = idx_permuted[:num_train_per_class]
             val_idx = idx_permuted[num_train_per_class:num_train_per_class + num_val_per_class]
             test_idx = idx_permuted[num_train_per_class + num_val_per_class:]
@@ -125,7 +137,14 @@ class GraphDataset(object):
         return masks
 
     # converting graph edge index representation to graph array list representation
-    def _setReversedArrayList(self, data):
+    def _setReversedArrayList(self, data: torch_geometric.data.Data):
+        """
+            creates a reversed array list from the edges
+
+            Parameters
+            ----------
+            data: torch_geometric.data.Data
+        """
         edge_index = data.edge_index
         num_nodes = data.num_nodes
         reversed_arr_list = [[] for _ in range(num_nodes)]
